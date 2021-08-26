e0941177e605bba9733e1218ccf799a16214789c
Silvio Ferreira de Souza Junior <silvio.junior@orsegups.com.br>
2021-08-26 13:30:49 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/codewaiter/cursomc/CursomcApplication.java
+++ src/main/java/com/codewaiter/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.codewaiter.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.codewaiter.cursomc.domain.Cidade;
 import com.codewaiter.cursomc.domain.Cliente;
 import com.codewaiter.cursomc.domain.Endereco;
 import com.codewaiter.cursomc.domain.Estado;
+import com.codewaiter.cursomc.domain.Pagamento;
+import com.codewaiter.cursomc.domain.PagamentoComBoleto;
+import com.codewaiter.cursomc.domain.PagamentoComCartao;
+import com.codewaiter.cursomc.domain.Pedido;
 import com.codewaiter.cursomc.domain.Produto;
+import com.codewaiter.cursomc.domain.enums.EstadoPagamento;
 import com.codewaiter.cursomc.domain.enums.TipoCliente;
 import com.codewaiter.cursomc.repositories.CategoriaRepository;
 import com.codewaiter.cursomc.repositories.CidadeRepository;
 import com.codewaiter.cursomc.repositories.ClienteRepository;
 import com.codewaiter.cursomc.repositories.EnderecoRepository;
 import com.codewaiter.cursomc.repositories.EstadoRepository;
+import com.codewaiter.cursomc.repositories.PagamentoRepository;
+import com.codewaiter.cursomc.repositories.PedidoRepository;
 import com.codewaiter.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -26,7 +34,7 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
@@ -42,10 +50,23 @@ public class CursomcApplication implements CommandLineRunner{
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired 
+	private PagamentoRepository pagamentoRepository;
+	
+	
+	
+	
+// -------------------------------- MAIN --------------------------------	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+// -------------------------------- MAIN --------------------------------
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -96,6 +117,27 @@ public class CursomcApplication implements CommandLineRunner{
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 //		-------------------------------------------------------		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+//		-------------------------------------------------------
+		
+		
+		
 		
 		
 	}
