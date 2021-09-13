249d61cb2b356370dcef6043c7fe6d04beb5466d
Silvio Ferreira de Souza Junior <silvio.junior@orsegups.com.br>
2021-09-13 17:45:33 -0300

    Ajustando e testando a delecao de cliente

--- src/main/java/com/codewaiter/cursomc/services/ClienteService.java
+++ src/main/java/com/codewaiter/cursomc/services/ClienteService.java
@@ -80,7 +80,7 @@ public class ClienteService {
 		try {
 			clienteRepository.deleteById(id);
 		}catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas!");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados!");
 		}
 	}
 
