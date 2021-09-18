9867fd7d3fc24ea246e35f519a23dd5d662db3a1
Silvio Junior <silvio.fs.junior@gmail.com>
2021-09-18 10:12:31 -0300

    Implementando autorizacao

--- src/main/java/com/codewaiter/cursomc/config/SecurityConfig.java
+++ src/main/java/com/codewaiter/cursomc/config/SecurityConfig.java
@@ -19,57 +19,54 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.codewaiter.cursomc.security.JWTAuthenticationFilter;
+import com.codewaiter.cursomc.security.JWTAuthorizationFilter;
 import com.codewaiter.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private JWTUtil jwtUtil;
-	
-	private static final String[] PUBLIC_MATCHERS = {
-			"/h2-console/**"
-	};
-	
-	private static final String[] PUBLIC_MATCHERS_GET_ONLY = {
-			"/produtos/**",
-			"/categorias/**"
-	};
-	
+
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
+	private static final String[] PUBLIC_MATCHERS_GET_ONLY = { "/produtos/**", "/categorias/**" };
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
+
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET_ONLY).permitAll()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET_ONLY).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
+
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-	
+
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
--- src/main/java/com/codewaiter/cursomc/security/JWTUtil.java
+++ src/main/java/com/codewaiter/cursomc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -17,13 +18,43 @@ public class JWTUtil {
 	@Value("${jwt.expiration}")
 	private Long jwtExpiration;
 
-	
 	public String generateToken(String login) {
-		return Jwts.builder()
-				.setSubject(login)
-				.setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
-				.signWith(SignatureAlgorithm.HS512, jwtSecret.getBytes())
-				.compact();
+		return Jwts.builder().setSubject(login).setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+				.signWith(SignatureAlgorithm.HS512, jwtSecret.getBytes()).compact();
 	}
-	
+
+	public boolean tokenValido(String token) {
+
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if (username != null & expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+
+		try {
+			return Jwts.parser().setSigningKey(jwtSecret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+	public String getUsername(String token) {
+
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+
 }
