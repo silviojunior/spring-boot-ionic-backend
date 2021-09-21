b5ef662843161d4640334cd89432154af2fd9c50
Silvio Ferreira de Souza Junior <silvio.junior@orsegups.com.br>
2021-09-21 15:05:24 -0300

    Enviando imagem via endpoint

--- src/main/java/com/codewaiter/cursomc/resources/ClienteResource.java
+++ src/main/java/com/codewaiter/cursomc/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.codewaiter.cursomc.domain.Cliente;
@@ -80,6 +81,13 @@ public class ClienteResource {
 		cliente = clienteService.insert(cliente);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
 		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PostMapping(value="/picture")
+	public ResponseEntity<URI> uploadProfilePicture(@RequestParam(name="file") MultipartFile multipartFile){
+		
+		URI uri = clienteService.uploadProfilePicture(multipartFile);
 		return ResponseEntity.created(uri).build();
 	}
 	
--- src/main/java/com/codewaiter/cursomc/services/ClienteService.java
+++ src/main/java/com/codewaiter/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.codewaiter.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.codewaiter.cursomc.domain.Cidade;
 import com.codewaiter.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id){
 		
@@ -112,6 +117,10 @@ public class ClienteService {
 		enderecoRepository.saveAll(cliente.getEnderecos());
 		
 		return cliente;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 	
 }
